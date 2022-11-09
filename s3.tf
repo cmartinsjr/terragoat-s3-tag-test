6a4f9c3e36592e12ac72fff2f44fb403257d4bf0
PALOALTONETWORKS\cmartinsjr <cmartinsjr@paloaltonetworks.com>
2022-11-09 13:12:23 -0500

    performed yor tagging to add the yor_trace tag

--- s3.tf
+++ s3.tf
@@ -1,11 +1,15 @@
 resource "aws_s3_bucket_acl" "data" {
-  bucket        = "${local.resource_prefix.value}-data"
-  acl           = "private"
+  bucket = "${local.resource_prefix.value}-data"
+  acl    = "private"
 }
 resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
+  bucket = "${local.resource_prefix.value}-data"
+  tags = {
+    yor_trace = "ccf11b7f-e248-4980-bdb7-84ab30a83244"
+    owner = "cmartinsjr_paloaltonetworks_com"
+  }
 }
