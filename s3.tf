fc521b7b35fbd93d7305affb894054b4e9b6b74d
cmartinsjr <103141817+cmartinsjr@users.noreply.github.com>
2022-12-20 16:20:31 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -8,10 +8,6 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket = "${local.resource_prefix.value}-data"
-  tags = {
-    yor_trace = "ccf11b7f-e248-4980-bdb7-84ab30a83244"
-    owner = "cmartinsjr_paloaltonetworks_com"
-  }
 }
 resource "aws_s3_bucket_public_access_block" "data" {
   bucket = "${local.resource_prefix.value}-data"
